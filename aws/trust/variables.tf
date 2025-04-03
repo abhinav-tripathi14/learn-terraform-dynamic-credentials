7313b5bfe492a1cfabd3348295ad10e9e5f4b81e
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-04-03 23:22:18 +0530

    Update variables.tf

--- aws/trust/variables.tf
+++ aws/trust/variables.tf
@@ -14,7 +14,7 @@ variable "tfc_organization_name" {
   type        = string
   description = "The name of your Terraform Cloud organization"
 }
-
+/*
 variable "tfc_project_name" {
   type        = string
   default     = "Default Project"
@@ -26,7 +26,7 @@ variable "tfc_workspace_name" {
   default     = "learn-terraform-dynamic-credentials-1"
   description = "The name of the workspace that you'd like to create and connect to AWS"
 }
-
+*/
 variable "aws_region" {
   type        = string
   default     = "us-east-2"
