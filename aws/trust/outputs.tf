39c80491984fb1edd0673538a38679cc809480dc
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-04-04 07:24:55 +0530

    Update outputs.tf

--- aws/trust/outputs.tf
+++ aws/trust/outputs.tf
@@ -7,3 +7,11 @@ output "role_arn" {
   description = "ARN for trust relationship role"
   value       = aws_iam_role.tfc_role.arn
 }
+
+output "aws_iam_openid_connect_provider"{
+  value = aws_iam_openid_connect_provider.tfc_provider.arn
+}
+
+output "client_id_list"{
+  value = aws_iam_openid_connect_provider.tfc_provider.client_id_list
+}
