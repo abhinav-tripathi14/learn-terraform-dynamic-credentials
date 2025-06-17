d586ced4af2b5e76668d92ee8103e35ce9f96ed8
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-17 11:17:11 +0530

    Update main.tf

--- aws/infra/main.tf
+++ aws/infra/main.tf
@@ -30,3 +30,21 @@ resource "aws_instance" "web" {
     name = "Dynamic_cred_ec2"
   }
 }
+resource "aws_instance" "web-1" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
+
+  #tags = var.tags
+    tags = {
+    name = "Dynamic_cred_ec2_2"
+  }
+}
