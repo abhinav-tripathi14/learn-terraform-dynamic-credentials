4336c2d539b6bca2259e4dda909f9ef3e1e106e2
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-18 14:09:23 +0530

    Update main.tf

--- aws/trust/main.tf
+++ aws/trust/main.tf
@@ -30,8 +30,8 @@ resource "aws_iam_role" "tfc_role" {
      "Action": "sts:AssumeRoleWithWebIdentity",
      "Condition": {
        "StringEquals": {
-         "${var.tfc_hostname}:aud": "${one(aws_iam_openid_connect_provider.tfc_provider.client_id_list)}",
-         "${var.tfc_hostname}:terraform_organization_name: "Test-Abhinav-1" 
+         "${var.tfc_hostname}:aud": "${one(aws_iam_openid_connect_provider.tfc_provider.client_id_list)}"
+         "${var.tfc_hostname}:terraform_organization_name: "Test-Abhinav" 
        },
        "ForAnyValue:StringLike": {
           "${var.tfc_hostname}:sub": "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}*:workspace:*:run_phase:*"
