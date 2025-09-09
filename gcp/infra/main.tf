e1712de0c595aa5c1962446572b11abe1f7a0aa4
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-09-09 13:31:29 +0530

    Update main.tf

--- gcp/infra/main.tf
+++ gcp/infra/main.tf
@@ -31,22 +31,3 @@ resource "google_compute_instance" "vm_instance" {
 
   tags = var.tags
 }
-
-resource "google_compute_backend_bucket" "image_backend" {
-  name        = "image-backend-bucket"
-  description = "Contains beautiful images"
-  bucket_name = google_storage_bucket.image_backend.name
-  enable_cdn  = true
-  edge_security_policy = google_compute_security_policy.policy.id
-}
-
-resource "google_storage_bucket" "image_backend" {
-  name     = "image-store-bucket"
-  location = "EU"
-}
-
-resource "google_compute_security_policy" "policy" {
-  name        = "image-store-bucket"
-  description = "basic security policy"
-    type = "CLOUD_ARMOR_EDGE"
-}
