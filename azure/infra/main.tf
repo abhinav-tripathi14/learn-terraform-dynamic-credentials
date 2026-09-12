0c7025ea9ea477688e6b3557de87384bff95f777
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-09-12 20:21:09 +0530

    Update main.tf

--- azure/infra/main.tf
+++ azure/infra/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "azure/azapi"
+    }
+   azurerm = {
+      source  = "hashicorp/azurerm"
+  }
+}
+}
+
 provider "azurerm" {
   features {
     resource_group {
