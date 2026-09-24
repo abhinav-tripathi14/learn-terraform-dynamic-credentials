866d84ed04d9beb55ffb028083679d4c2f71f098
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-09-24 11:56:51 +0530

    Update main.tf

--- azure/infra/main.tf
+++ azure/infra/main.tf
@@ -4,15 +4,15 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
 }
-  #subscription_id = "57ad2e58-e56c-4ba8-b326-f8a11cf2781a"
-  #client_id       = "9efe8bf7-32da-41c4-8f78-c070f7dc38d9"
+  subscription_id = "57ad2e58-e56c-4ba8-b326-f8a11cf2781a"
+  client_id       = "9efe8bf7-32da-41c4-8f78-c070f7dc38d9"
   tenant_id       = "56f775a3-2540-4f05-ab58-72cd72d17d3e"
   use_oidc        = true
   }
 
 provider "azapi" {
-  #subscription_id = "57ad2e58-e56c-4ba8-b326-f8a11cf2781a"
-  #client_id       = "9efe8bf7-32da-41c4-8f78-c070f7dc38d9"
+  subscription_id = "57ad2e58-e56c-4ba8-b326-f8a11cf2781a"
+  client_id       = "9efe8bf7-32da-41c4-8f78-c070f7dc38d9"
   tenant_id       = "56f775a3-2540-4f05-ab58-72cd72d17d3e"
   use_oidc = true
 }
